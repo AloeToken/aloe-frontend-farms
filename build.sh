02096a01051dfc9d8ecb89f4d8dbbad94c617f73
AloeSwap <info@aloeswap.com>
2021-05-30 23:28:39 +0200

    New favicon, tether icon, build script and package name

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+read -r -p "Upgrade dependencies? [y/N] " dep
+if [ "$dep" = "Y" ] || [ "$dep" = "y" ]
+then
+    yarn upgrade
+fi
+
 rm -r ./build/*
 npm run-script build
 rm -r /data/www/test.aloeswap.com/*
